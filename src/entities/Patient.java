8ee59802dc48073e0d46fc447598a3a3b69f1ea2
CarolinaCedro <yotechgirl@gmail.com>
2022-07-15 22:43:33 -0300

    feat: init project medical records

---
+++ src/entities/Patient.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Patient {
+
+    private String FullName;
+    private Integer Age;
+    private Integer Id;
+    private Double BloodPressure;
+    private String BloodType;
+
+}
